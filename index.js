494ee578532e6ef3f5eb417e7bdc520c31f2aca9
JBWERE-coder <lutherwere@gmail.com>
2023-07-02 11:35:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const complementSet = new Set();
+
+  for (let num of array) {
+    const complement = target - num;
+    if (complementSet.has(complement)) {
+      return true;
+    }
+    complementSet.add(num);
+  }
+
+  return false;
 }
+
+/* Write your algorithm here
+1. define the function hasTargetSum with two parameters: arr (an array of integers) and target (the target integer).
+2. Create an empty set called visitedNumbers to store the numbers we have encountered in the array.
+3. Iterate through each number (num) in the array:
+    - Calculate the difference by subtracting num from the target.
+    - Check if the difference is already in the visitedNumbers set:
+        - If it is, return true since we have found a pair that adds up to the target.
+    - Add the current num to the visitedNumbers set.
+4. If the loop completes without finding a pair, return false since no pair adds up to the target.
 
 /* 
   Write the Big O time complexity of your function here
+  The time complexity of the hasTargetSum function is O(n), where n is the size of the input array.
 */
 
 /* 
   Add your pseudocode here
+  1. define the function hasTargetSum with two parameters: arr (an array of integers) and target (the target integer).
+2. Create an empty set called visitedNumbers to store the numbers we have encountered in the array.
+3. Iterate through each number (num) in the array:
+    - Calculate the difference by subtracting num from the target.
+    - Check if the difference is already in the visitedNumbers set:
+        - If it is, return true since we have found a pair that adds up to the target.
+    - Add the current num to the visitedNumbers set.
+4. If the loop completes without finding a pair, return false since no pair adds up to the target.
+
 */
 
 /*
   Add written explanation of your solution here
+  The pseudocode provides a high-level description of the steps involved in the hasTargetSum function.
+   It outlines the necessary actions to check if any pair of numbers in the array adds up to the target.
 */
 
 // You can run `node index.js` to view these console logs
